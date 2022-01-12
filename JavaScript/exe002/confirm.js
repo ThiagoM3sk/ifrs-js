745d484dd1bd216bda699bc6a8568ccf2f625d96
Thiago Mesquita <83050372+ThiagoM3sk@users.noreply.github.com>
2022-01-11 23:36:54 -0300

    commit do exercício02

---
+++ JavaScript/exe002/confirm.js
@@ -0,0 +1,6 @@
+var sair = confirm("Você confirma ir para a página do Google?")
+
+if(sair == 1){
+    window.location = "https://www.google.com.br"
+}
+// no JavaScript, o 1 representa positivo (true) e 0 representa negativo (false)

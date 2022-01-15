87751bf728e8d002731ec9b8d181136d49d8a79f
Thiago Mesquita <83050372+ThiagoM3sk@users.noreply.github.com>
2022-01-14 22:09:24 -0300

    exercícios sobre eventos parte 1

---
+++ JavaScript/exe005/click.js
@@ -0,0 +1,8 @@
+function umClique(){
+    document.getElementById('imagem').src="fachada.jpg"
+    document.getElementById('log').innerHTML += 'Trocou para foto da fachada<br>'
+}
+function doisCliques(){
+    document.getElementById('imagem').src= "ifrs.gif"
+    document.getElementById('log').innerHTML += 'Trocou para a logo do IFRS-BG<br>'
+}
---
+++ JavaScript/exe005/load.js
@@ -0,0 +1,6 @@
+function carreguei(){
+    document.getElementById('log').innerHTML += 'Carreguei<br>' 
+}
+function redimensionei(){
+    document.getElementById('log').innerHTML += 'Redimensionei<br>'
+}
